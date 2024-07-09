1f3fe18bdfcd475f601cfb70cd6b287d76feb066
Eldor Karimov <hacktivist19021@gmail.com>
2024-07-09 18:05:59 +0500

    send Otp code using signals

--- account/signals.py
+++ account/signals.py
@@ -1,13 +1,18 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from account.models import User, VerificationOtp
-from account.utils import code_generate
+from account.utils import code_generate, send_email
 from django.utils import timezone
 from config.settings.base import EXPIRES_OTP_CODE
 
 @receiver(post_save, sender=User)
-def create_user(sender, instance, created, **kwargs):
+def save_code(sender, instance, created, **kwargs):
     if created:
         code = code_generate()
-        VerificationOtp.objects.create(user=instance, code=code, type=VerificationOtp.VerificationType.REGISTER,
-                        expires_in=timezone.now() + timezone.timedelta(minutes=EXPIRES_OTP_CODE))
+        VerificationOtp.objects.create(
+            user = instance, 
+            code = code,
+            type = VerificationOtp.VerificationType.REGISTER,
+            expires_in = timezone.now() + timezone.timedelta(minutes=EXPIRES_OTP_CODE)
+        )
+        send_email(code=code, email=instance.email, user=instance)
--- account/utils.py
+++ account/utils.py
@@ -1,7 +1,16 @@
 import random
+from django.core.mail import send_mail
+
+from config.settings import base
 
 def code_generate():
-    return ''.join([random.randint(1000, 100000) % 10 for i in range(6)])
+    nums = [1,2,3,4,5,6,7,8,9,0]
+    return ''.join([str(random.choice(nums)) for i in range(6)])
 
-def send_email():
-    pass
+def send_email(code, email, user):
+    send_mail(
+        subject=f"Hello {user.get_full_name}",
+        message=f"your activation code is {code}",
+        from_email=base.EMAIL_HOST_USER,
+        recipient_list=[email]
+    )
